92886b085d348099338bf83f91366630e8d3abb7
Perkles <joaohigor_fortunato@hotmail.com>
2022-06-11 11:59:39 -0300

    Segundo Commit

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
-function btnCliked(){
-    const todoInput = document.getElementById('todoInput');
+const todoInput = document.getElementById('todoInput');
+
+todoInput.addEventListener('keyup', onTextEnter);
+
+function onTextEnter(event){
+    if(event.key != 'Enter' || event.keyCode != 13 || todoInput.value.trim() === "" ){
+        return;
+    }
+
     const list = document.getElementById('list');    
     const inputValue = todoInput.value;
 
     const newTodo = document.createElement('li');
+    newTodo.classList.add('row');
+
     const newContent = document.createTextNode(inputValue);
 
     newTodo.appendChild(newContent);
     list.appendChild(newTodo);
+}
+
+
     
 
-}
+
+
+//function btnCliked(){
+//    const todoInput = document.getElementById('todoInput');
+
+
+//    const list = document.getElementById('list');    
+//    const inputValue = todoInput.value;
+
+//    const newTodo = document.createElement('li');
+//    newTodo.classList.add('row');
+
+//    const newContent = document.createTextNode(inputValue);
+
+//    newTodo.appendChild(newContent);
+//    list.appendChild(newTodo);
+    
+
+//}
